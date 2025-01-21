605aacbd76100732f04d992acf717fb5593c0996
PSNAppZ <thepsnarayanan@gmail.com>
2025-01-21 10:25:46 +0530

    pre-commit fixes

--- openg2p-registry-bg-tasks-models/src/openg2p_registry_bg_tasks_models/models/g2p_que_background_tasks.py
+++ openg2p-registry-bg-tasks-models/src/openg2p_registry_bg_tasks_models/models/g2p_que_background_tasks.py
@@ -3,9 +3,8 @@ from datetime import datetime
 
 from openg2p_fastapi_common.models import BaseORMModel
 from sqlalchemy import JSON, DateTime, Integer, String
-from sqlalchemy.orm import mapped_column
 from sqlalchemy import Enum as SqlEnum
-
+from sqlalchemy.orm import mapped_column
 
 
 class TaskStatus(enum.Enum):
@@ -20,7 +19,9 @@ class G2PQueBackgroundTask(BaseORMModel):
     id = mapped_column(Integer, primary_key=True, autoincrement=True)
     worker_type = mapped_column(String, default="example_worker")  # Default worker type
     worker_payload = mapped_column(JSON, nullable=False)
-    task_status = mapped_column(SqlEnum(TaskStatus), nullable=False, default=TaskStatus.PENDING)
+    task_status = mapped_column(
+        SqlEnum(TaskStatus), nullable=False, default=TaskStatus.PENDING
+    )
     queued_datetime = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
     number_of_attempts = mapped_column(Integer, default=0)
     last_attempt_datetime = mapped_column(DateTime, nullable=True)
